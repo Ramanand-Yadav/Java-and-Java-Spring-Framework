95ab6050b5a4772c02b00fd41c46b116d427e806
Ramanand-Yadav <1112000yadav@gmail.com>
2024-01-30 01:38:42 +0530

    hello World in Java

---
+++ BasicJava/helloWord.java
@@ -0,0 +1,10 @@
+package BasicJava;
+
+public class helloWord{
+    public static void main(String[] args) {
+        System.out.println("Ram");
+        System.out.println("Ramanand Yadav");
+        System.out.println("vikas");
+        System.out.println("updated");
+    }
+}
