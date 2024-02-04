9d55c635192678d0eeede83362aae04ffd34cf9a
Ramanand-Yadav <1112000yadav@gmail.com>
2024-02-04 17:56:09 +0530

    JDBC Connection

--- BasicJava/HelloWord/Main.java
+++ BasicJava/HelloWord/Main.java
@@ -1,7 +1,30 @@
 package BasicJava.HelloWord;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
 public class Main{
     public static void main(String[] args) {
         System.out.println("Ramaand Yadav");
+        SteamAPI();
+    }
+    public static void SteamAPI(){
+        List<Integer>nums = Arrays.asList(5,7,2,5,7,8);
+        
+        // for(int i=0; i<nums.size(); i++){
+        //     System.out.println(nums.get(i));
+        // }
+        Consumer<Integer> con = n->System.out.println(n);
+            
+    
+        // nums.forEach(n->System.out.println(n));
+        // nums.forEach(con);
+
+        Stream<Integer> s1 = nums.stream();
+        s1.forEach(con);
+        //Steam can't be reused
+        
     }
 }
---
+++ JDBCConnection/src/Main.java
@@ -0,0 +1,40 @@
+import java.sql.*;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        System.out.println("Hello world!");
+        /*
+        import package
+        load and register
+        create connection
+        create statement
+        execute statement
+        process the result
+        close
+         */
+        String url = "jdbc:postgresql://localhost:5432/Demo";
+        String uname = "postgres";
+        String pass = "Shiva#$9812";
+        String sql = "select sname from student where sid=1";
+        Class.forName("org.postgresql.Driver");
+        Connection con = DriverManager.getConnection(url,uname,pass);
+        System.out.println("Connection Establish");
+
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+//        rs.next();
+       // while(rs.next()){
+        rs.next();
+            String name = rs.getString("sname");
+            int id = rs.getInt("sid");
+            int mark = rs.getInt("mark");
+            System.out.println(id+" "+name+" "+mark);
+            rs.next();
+
+//        String name = rs.getString("sname");
+//        System.out.println(name);
+        con.close();
+
+        System.out.println("Connection Closed");
+
+    }
+}
